63e65be9a6e562e93d0639930fd228b9d7fc72e9
Clemens Reijnen <clemensreijnen@live.nl>
2014-06-11 19:33:40 +0200

    load rooms

---
+++ VSOTeams/VSOTeams/VSOTeams/Views/RoomMessagesView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VSOTeams.Models;
+
+namespace VSOTeams.Views
+{
+    public class RoomMessagesView : BaseView
+    {
+        public RoomMessagesView(TeamRoom room)
+		{
+            //BindingContext = item;
+            //var webView = new WebView ();
+            //webView.Source = new HtmlWebViewSource {
+            //    Html = item.Description
+            //};
+            //Content = webView;
+		}
+    }
+}
